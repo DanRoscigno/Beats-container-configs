7cddcba2be418e79d50bc4ca0fc6e12d29df4c1d
DanRoscigno <dan@roscigno.com>
2018-11-26 22:42:29 -0500

    moved setup to separate command

--- step06-metricbeat.sh
+++ step06-metricbeat.sh
@@ -1,6 +1,6 @@
 docker run \
 --net course_stack \
---net host docker.elastic.co/beats/metricbeat:6.5.1 \
+docker.elastic.co/beats/metricbeat:6.5.1 \
 setup -E setup.kibana.host=kibana:5601 \
 -E output.elasticsearch.hosts=["elasticsearch:9200"]
 
