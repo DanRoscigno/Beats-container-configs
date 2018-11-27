b56bc8e9a12b9b3f70f127e2b0228cf7c304f978
DanRoscigno <dan@roscigno.com>
2018-11-26 22:39:47 -0500

    changed args related to network

--- step07-packetbeat.sh
+++ step07-packetbeat.sh
@@ -1,13 +1,17 @@
 docker run \
 --cap-add=NET_ADMIN \
 --net course_stack \
---net host docker.elastic.co/beats/packetbeat:6.5.1 \
+docker.elastic.co/beats/packetbeat:6.5.1 \
 setup -E setup.kibana.host=kibana:5601 \
 -E output.elasticsearch.hosts=["elasticsearch:9200"]
 
 docker run -d \
---name=packetbeat \
---cap-add=NET_ADMIN  \
---volume "$(pwd)/configs/packetbeat.yml:/usr/share/packetbeat/packetbeat.yml:ro" \
-docker.elastic.co/beats/packetbeat:6.5.1 -e
+  --name=packetbeat \
+  --user=packetbeat \
+  --volume="$(pwd)/configs/packetbeat.yml:/usr/share/packetbeat/packetbeat.yml:ro" \
+  --cap-add="NET_RAW" \
+  --cap-add="NET_ADMIN" \
+  --network=host \
+  docker.elastic.co/beats/packetbeat:6.5.1 \
+  --strict.perms=false -e -E output.elasticsearch.hosts=http://localhost:9200
 
