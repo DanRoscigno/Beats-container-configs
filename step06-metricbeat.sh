3252fc55b8a580233c4ed1415f1a7eb6eeba964f
DanRoscigno <dan@roscigno.com>
2018-11-28 10:35:59 -0500

    added match discover

--- step06-metricbeat.sh
+++ step06-metricbeat.sh
@@ -1,6 +1,6 @@
 docker run \
 --net course_stack \
-docker.elastic.co/beats/metricbeat:6.4.2 \
+docker.elastic.co/beats/metricbeat:6.5.1 \
 setup -E setup.kibana.host=kibana:5601 \
 -E output.elasticsearch.hosts=["elasticsearch:9200"]
 
@@ -8,10 +8,21 @@ docker run -d \
 --net course_stack \
 --name=metricbeat \
 --user=root \
---volume="$(pwd)/configs/metricbeat.yml:/usr/share/metricbeat/metricbeat.yml:ro" \
+--volume="$(pwd)/hint-configs/metricbeat.yml:/usr/share/metricbeat/metricbeat.yml:ro" \
 --volume="/var/run/docker.sock:/var/run/docker.sock:ro" \
 --volume="/sys/fs/cgroup:/hostfs/sys/fs/cgroup:ro" \
 --volume="/proc:/hostfs/proc:ro" \
 --volume="/:/hostfs:ro" \
-docker.elastic.co/beats/metricbeat:6.4.2 metricbeat -e
+docker.elastic.co/beats/metricbeat:6.5.1 metricbeat -e
+
+docker run -d \
+--net course_stack \
+--name=metricbeat \
+--user=root \
+--volume="$(pwd)/match-configs/metricbeat.yml:/usr/share/metricbeat/metricbeat.yml:ro" \
+--volume="/var/run/docker.sock:/var/run/docker.sock:ro" \
+--volume="/sys/fs/cgroup:/hostfs/sys/fs/cgroup:ro" \
+--volume="/proc:/hostfs/proc:ro" \
+--volume="/:/hostfs:ro" \
+docker.elastic.co/beats/metricbeat:6.5.1 metricbeat -e
 
