190bf4a2a03aec06a211f9a7b655701bf3832a9c
DanRoscigno <dan@roscigno.com>
2018-11-28 10:15:00 -0500

    added match discovery

--- step05-filebeat.sh
+++ step05-filebeat.sh
@@ -9,7 +9,16 @@ docker run -d \
 --name=filebeat \
 --user=root \
 --volume="/var/lib/docker/containers:/var/lib/docker/containers:ro" \
---volume="$(pwd)/configs/filebeat.yml:/usr/share/filebeat/filebeat.yml:ro" \
+--volume="$(pwd)/hint-configs/filebeat.yml:/usr/share/filebeat/filebeat.yml:ro" \
+--volume="/var/run/docker.sock:/var/run/docker.sock:ro" \
+docker.elastic.co/beats/filebeat:6.4.2 filebeat -e -strict.perms=false
+
+docker run -d \
+--net course_stack \
+--name=filebeat \
+--user=root \
+--volume="/var/lib/docker/containers:/var/lib/docker/containers:ro" \
+--volume="$(pwd)/match-configs/filebeat.yml:/usr/share/filebeat/filebeat.yml:ro" \
 --volume="/var/run/docker.sock:/var/run/docker.sock:ro" \
 docker.elastic.co/beats/filebeat:6.4.2 filebeat -e -strict.perms=false
 
